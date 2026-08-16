2d6f03b6274370a6b17cdee8e58a0ee0c78fc5c0
AyeshaBhatti-9 <162040733+AyeshaBhatti-9@users.noreply.github.com>
2026-08-17 01:18:13 +0500

    Delete BACKEND/data_collection directory

--- BACKEND/data_collection/csv_to_json.py
+++
@@ -1,9 +0,0 @@
-import csv
-import json
-
-with open('data/germany_knowledge.csv', 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    data = list(csv_reader)
-
-with open('germany_knowledge.json', 'w') as json_file:
-    json.dump(data, json_file, indent=4)
